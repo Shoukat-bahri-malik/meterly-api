0b5db75604998559d4ac5be1e736cf60f0cad6a4
Shoukat Malik <shoukat.malik@danatonline.com>
2025-09-08 12:50:48 +0500

    shift connection code to db.config.js

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const {ConnectDB } = require('./db/db.config');
 
-
+ConnectDB();
 const app = express();
 app.use(express.json());
 app.use('/api/users', require('./routes/user.routes'));
 
-mongoose.connect("mongodb+srv://apiuser:api123456@mybackend.utdk5bw.mongodb.net/?retryWrites=true&w=majority&appName=mybackend")
-.then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
-})
-.catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
 
 
 
