5be54f9cfa71830a9d36b5da552a535073c44175
Shoukat Malik <shoukat.malik@danatonline.com>
2025-09-08 12:40:30 +0500

    adding db configs

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { connectDB } = require('./db/db.config');
+const {ConnectDB } = require('./db/db.config');
 
 
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(express.json());
 app.use('/api/users', require('./routes/user.routes'));
 
-connectDB();
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+mongoose.connect("mongodb+srv://apiuser:api123456@mybackend.utdk5bw.mongodb.net/?retryWrites=true&w=majority&appName=mybackend")
+.then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
 });
+
+
+
+
+
 
 
